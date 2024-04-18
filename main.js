bcef9dfe3cf30b52e583dc58895884137769e4b2
Ben Rollin <ben.rollin@clever.com>
2024-04-18 01:41:20 -0700

    do color pallette things

--- main.js
+++ main.js
@@ -42,28 +42,33 @@ function init() {
 
   // initialize particles
 
-  const particleNum = 300000;
-  const particleSize = 4; // vec2
+  const particleCount = 300000;
 
   // create buffers
 
   const particleBuffer = new StorageInstancedBufferAttribute(
-    particleNum,
-    particleSize,
+    particleCount,
+    2, // vec2
   );
   const velocityBuffer = new StorageInstancedBufferAttribute(
-    particleNum,
-    particleSize,
+    particleCount,
+    2, // vec2
+  );
+  const colorBuffer = new StorageInstancedBufferAttribute(
+    particleCount,
+    3, // vec3
   );
 
-  const particleBufferNode = storage(particleBuffer, "vec2", particleNum);
-  const velocityBufferNode = storage(velocityBuffer, "vec2", particleNum);
+  const particleBufferNode = storage(particleBuffer, "vec2", particleCount);
+  const velocityBufferNode = storage(velocityBuffer, "vec2", particleCount);
+  const colorBufferNode = storage(colorBuffer, "vec3", particleCount);
 
   // create function
 
   const computeShaderFn = tslFn(() => {
     const particle = particleBufferNode.element(instanceIndex);
     const velocity = velocityBufferNode.element(instanceIndex);
+    const color = colorBufferNode.element(instanceIndex);
 
     const pointer = uniform(pointerVector);
     const limit = uniform(scaleVector);
@@ -81,6 +86,25 @@ function init() {
 
     position.assign(position.min(limit).max(limit.negate()));
 
+    const polarPosition = vec2(
+      position.length(),
+      position.y
+        .atan2(position.x)
+        .div(2 * Math.PI)
+        .add(0.5),
+    );
+
+    const a = vec3(0.261, 0.446, 0.315),
+      b = vec3(0.843, 0.356, 0.239),
+      c = vec3(0.948, 1.474, 1.361),
+      d = vec3(3.042, 5.63, 5.424);
+    // vec3 palette(in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d) {
+    //   return a + b * cos(6.28318 * (c * t + d));
+    // }
+    color.assign(
+      a.add(b.mul(polarPosition.x.mul(c).add(d).mul(6.28318).cos())),
+    );
+
     const pointerSize = 0.1;
     const distanceFromPointer = pointer.sub(position).length();
 
@@ -91,7 +115,7 @@ function init() {
 
   // compute
 
-  computeNode = computeShaderFn().compute(particleNum);
+  computeNode = computeShaderFn().compute(particleCount);
   computeNode.onInit = ({ renderer }) => {
     const precomputeShaderNode = tslFn(() => {
       const particleIndex = float(instanceIndex);
@@ -107,12 +131,13 @@ function init() {
       velocity.xy = vec2(velX, velY);
     });
 
-    renderer.compute(precomputeShaderNode().compute(particleNum));
+    renderer.compute(precomputeShaderNode().compute(particleCount));
   };
 
   // use a compute shader to animate the point cloud's vertex data.
 
   const particleNode = attribute("particle", "vec2");
+  const colorNode = attribute("color", "vec3");
 
   const pointsGeometry = new THREE.BufferGeometry();
   pointsGeometry.setAttribute(
@@ -120,15 +145,16 @@ function init() {
     new THREE.BufferAttribute(new Float32Array(3), 3),
   ); // single vertex ( not triangle )
   pointsGeometry.setAttribute("particle", particleBuffer); // dummy the position points as instances
+  pointsGeometry.setAttribute("color", colorBuffer);
   pointsGeometry.drawRange.count = 1; // force render points as instances ( not triangle )
 
   const pointsMaterial = new PointsNodeMaterial();
-  pointsMaterial.colorNode = particleNode.add(color(0xff0000));
+  pointsMaterial.colorNode = colorNode.add(color(0x000000));
   pointsMaterial.positionNode = particleNode;
 
   const mesh = new THREE.Points(pointsGeometry, pointsMaterial);
   mesh.isInstancedMesh = true;
-  mesh.count = particleNum;
+  mesh.count = particleCount;
   scene.add(mesh);
 
   renderer = new WebGPURenderer({ antialias: true });
